a337ed5ae8fba41a13e462e4a7310a407db639a5
JWinegar06 <winegaj86@gmail.com>
2026-02-02 16:48:05 -0700

    Updates

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
-  return mongoose
-    .connect(url)
-    .then(() => {
-      console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
 };
 
 module.exports = connectDB;
